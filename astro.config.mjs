c074935c31736d2a234f4618d4648834a2b4ec43
corust-ci <ci@corust.ai>
2026-04-13 00:55:46 +0000

    feat: Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+const site = process.env.SITE_URL?.trim() || 'https://lester-new-blog.fly.dev';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lester-new-blog.fly.dev',
+  site,
   integrations: [tailwind(), sitemap()],
 });
